ad444657a231917824b835e2c510d6c206fc2857
Asoka Wotulo <asokawotulo@yahoo.com>
2020-06-10 09:33:32 +0700

    fix: scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\OutgoingRecommendation'
     ];
 
     /**
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(OutgoingRecommendation::class)
+        $schedule->command('recommendations:queue')
             ->dailyAt('12:30')
             ->timezone('Australia/Sydney')
             ->weekdays()
